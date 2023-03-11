8cf4a7cf85b7aa67de114cd8d30a6959912243e7
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-03-11 12:04:58 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Navbar from './Navbar/Navbar';
 import { Routes, Route } from 'react-router-dom'
 import Home from './Home/Home'
-import Footer from './compenents/footer';
+import Footer from './compenents/Footer';
 import ListProject from './ListProject/ListProject';
 
 function App() {
   return (
     <div className="App">
-
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
